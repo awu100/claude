b19e5bddf7f5df5ea338b65483b49124fed0c1d3
Colin Tindle <colin.tindle@fanduel.com>
2019-07-18 22:37:02 +0100

    add !uptime command

--- src/index.js
+++ src/index.js
@@ -4,16 +4,39 @@ require("dotenv").config();
 
 client.once("ready", () => {
   console.log("Connected to discord");
+  const loadTime = Date.now();
 
   client.on("message", message => {
-    console.log(`<@${client.user.id}> ${message.content}`);
+    // console.log(`<@${client.user.id}> ${message.content}`);
     if (message.channel.type !== "dm" || message.author.bot) {
       return;
     }
 
-    const user = message.author.username;
+    // const user = message.author.username;
     const text = message.content;
-    message.channel.send(`Your message was "${text}"`);
+    const breakdown = text.split(" ");
+
+    const command = breakdown.length > 1 ? breakdown[0] : text;
+
+    switch (command) {
+      case "!uptime":
+        const seconds = (Date.now() - loadTime) / 1000;
+        const hours = seconds / 3600;
+
+        if (seconds < 3600) {
+          const minutes = parseInt(seconds / 60);
+          const secs = parseInt(seconds - minutes / (seconds % 60));
+          message.channel.send(
+            `I've been up for ${minutes} minutes and ${secs} seconds.`
+          );
+        } else {
+          message.channel.send(`I've been up for ${hours} hours.`);
+        }
+        break;
+
+      default:
+        break;
+    }
   });
 });
 
