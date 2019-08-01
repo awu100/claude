e437b1886708c68a1d34258128980d5ab127eebc
Colin Tindle <colin.tindle@fanduel.com>
2019-08-01 16:49:51 +0100

    rename session to sessions

--- src/bot/commands/sale.js
+++ src/bot/commands/sale.js
@@ -1,16 +1,17 @@
 module.exports = ({ params, message, client }) => {
-  if (message.channel.name !== "session-chat" || !params) {
-    return;
-  }
+    if (message.channel.name !== "sessions-chat" || !params) {
+        return
+    }
 
-  const salesQueue = client.channels.find(
-    channel =>
-      channel.name === "sales-queue" && channel.parent == message.channel.parent
-  );
+    const salesQueue = client.channels.find(
+        channel =>
+            channel.name === "sales-queue" &&
+            channel.parent == message.channel.parent
+    )
 
-  if (!salesQueue) {
-    return;
-  }
+    if (!salesQueue) {
+        return
+    }
 
-  salesQueue.send(`${message.author}: ${params}`);
-};
+    salesQueue.send(`${message.author}: ${params}`)
+}
--- src/bot/commands/sale.test.js
+++ src/bot/commands/sale.test.js
@@ -1,57 +1,57 @@
-const sale = require("./sale");
+const sale = require("./sale")
 
 describe("Sale", () => {
-  const client = {
-    channels: [
-      { name: "sales-queue", parent: "XB", send: jest.fn() },
-      { name: "sales-queue", parent: "PS", send: jest.fn() }
-    ]
-  };
+    const client = {
+        channels: [
+            { name: "sales-queue", parent: "XB", send: jest.fn() },
+            { name: "sales-queue", parent: "PS", send: jest.fn() }
+        ]
+    }
 
-  function messageWith({ name, parent } = {}) {
-    return {
-      author: "BananaMan",
-      channel: {
-        name: name || "session-chat",
-        parent: parent || "PS"
-      }
-    };
-  }
+    function messageWith({ name, parent } = {}) {
+        return {
+            author: "BananaMan",
+            channel: {
+                name: name || "sessions-chat",
+                parent: parent || "PS"
+            }
+        }
+    }
 
-  let message;
+    let message
 
-  beforeEach(() => {
-    jest.resetAllMocks();
-    message = messageWith();
-  });
+    beforeEach(() => {
+        jest.resetAllMocks()
+        message = messageWith()
+    })
 
-  test("posts sale to sales-queue", () => {
-    sale({ params: "2mc", message, client });
+    test("posts sale to sales-queue", () => {
+        sale({ params: "2mc", message, client })
 
-    expect(client.channels[1].send).toHaveBeenCalledWith("BananaMan: 2mc");
-    expect(client.channels[0].send).not.toHaveBeenCalled();
-  });
+        expect(client.channels[1].send).toHaveBeenCalledWith("BananaMan: 2mc")
+        expect(client.channels[0].send).not.toHaveBeenCalled()
+    })
 
-  test("no post because message was in the wrong queue", () => {
-    message = messageWith({ name: "something" });
-    sale({ params: "2mc", message, client });
+    test("no post because message was in the wrong queue", () => {
+        message = messageWith({ name: "something" })
+        sale({ params: "2mc", message, client })
 
-    expect(client.channels[1].send).not.toHaveBeenCalled();
-    expect(client.channels[0].send).not.toHaveBeenCalled();
-  });
+        expect(client.channels[1].send).not.toHaveBeenCalled()
+        expect(client.channels[0].send).not.toHaveBeenCalled()
+    })
 
-  test("no post because no parameters", () => {
-    sale({ params: null, message, client });
+    test("no post because no parameters", () => {
+        sale({ params: null, message, client })
 
-    expect(client.channels[1].send).not.toHaveBeenCalled();
-    expect(client.channels[0].send).not.toHaveBeenCalled();
-  });
+        expect(client.channels[1].send).not.toHaveBeenCalled()
+        expect(client.channels[0].send).not.toHaveBeenCalled()
+    })
 
-  test("no post because can't find saleQueue in matching category", () => {
-    message = messageWith({ parent: "something" });
-    sale({ params: "2mc", message, client });
+    test("no post because can't find saleQueue in matching category", () => {
+        message = messageWith({ parent: "something" })
+        sale({ params: "2mc", message, client })
 
-    expect(client.channels[1].send).not.toHaveBeenCalled();
-    expect(client.channels[0].send).not.toHaveBeenCalled();
-  });
-});
+        expect(client.channels[1].send).not.toHaveBeenCalled()
+        expect(client.channels[0].send).not.toHaveBeenCalled()
+    })
+})
