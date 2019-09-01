4ea6d3941738b0a861a9e5fcc87a00041013c724
Colin Tindle <underscoredotspace@users.noreply.github.com>
2019-09-01 17:57:09 +0100

    Feature/boot clock random (#8)
    * add boot and stopwatch
    
    * add extra message to explain reactions

--- src/bot/commands/random.js
+++ src/bot/commands/random.js
@@ -7,9 +7,11 @@ module.exports = ({ message, params }) => {
         ? `@here **Please kick \`${params}\`!\n\nDo __not__ split kicks.**`
         : `@here **Please kick the random!**`
 
+    message.channel.send(`:rotating_light: ${messageBody} :rotating_light:`)
     message.channel
-        .send(`:rotating_light: ${messageBody} :rotating_light:`)
-        .then(kickMessage => {
+        .send(
+            "React with :boot: if you have kicked or :stopwatch: if your kick is disabled"
+        ).then(kickMessage => {
             kickMessage.react("👢")
             kickMessage.react("⏱")
         })
